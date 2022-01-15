ff01b56785e1e4386bfb41a42da2fac537087a0a
vargasraquel <raquelvargas@gmail.com>
2022-01-15 16:19:21 -0300

    Final edits and deploy

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -1,19 +1,19 @@
-// const axios = require("axios");
-
-// module.exports = async function() {
-//   const result = await axios.get("https://api.github.com/users/vargasraquel/repos")
-
-//   return result.data
-// };
-
-const Cache = require("@11ty/eleventy-cache-assets");
+const axios = require("axios");
 
 module.exports = async function() {
-  let url = "https://api.github.com/users/vargasraquel/repos";
+  const result = await axios.get("https://api.github.com/users/vargasraquel/repos")
 
-  /* This returns a promise */
-  return Cache(url, {
-    duration: "2h", // save for 2 hours
-    type: "json"    // we’ll parse JSON for you
-  });
+  return result.data
 };
+
+// const Cache = require("@11ty/eleventy-cache-assets");
+
+// module.exports = async function() {
+//   let url = "https://api.github.com/users/vargasraquel/repos";
+
+//   /* This returns a promise */
+//   return Cache(url, {
+//     duration: "2h", // save for 2 hours
+//     type: "json"    // we’ll parse JSON for you
+//   });
+// };
