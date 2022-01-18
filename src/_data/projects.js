5e2d660c1746ea372ac97316bb7cbc49ce971f22
vargasraquel <raquelvargas@gmail.com>
2022-01-18 18:31:37 -0300

    fixing css in chrome and safari

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -5,15 +5,3 @@ module.exports = async function() {
 
   return result.data
 };
-
-// const Cache = require("@11ty/eleventy-cache-assets");
-
-// module.exports = async function() {
-//   let url = "https://api.github.com/users/vargasraquel/repos";
-
-//   /* This returns a promise */
-//   return Cache(url, {
-//     duration: "2h", // save for 2 hours
-//     type: "json"    // we’ll parse JSON for you
-//   });
-// };
